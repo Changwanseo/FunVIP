23e65bde2d04fb977f4fed45ef0c5844f89499f1
Changwanseo <64393882+Changwanseo@users.noreply.github.com>
2023-07-15 18:27:58 +0900

    0.3.12.0.1.0
    - Save before build

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ from sys import platform
 import subprocess
 
 name = "FunID"
-__version__ = "0.3.12"
-release = "0.3.12.0.0"
+__version__ = "0.3.12.1"
+release = "0.3.12.1.0"
 
 
 ## Default setup options
