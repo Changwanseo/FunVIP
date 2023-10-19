8c3eae907dc59a11fdc6b3c704b108dd49151f58
Changwan Seo <wan101010@snu.ac.kr>
2023-10-19 10:50:56 +0900

    0.3.18.2.1.0
    - Save before build

--- funid/src/reporter.py
+++ funid/src/reporter.py
@@ -9,7 +9,7 @@ from funid.src.tool import index_step
 from funid.src.save import save_df
 
 # For version reporting
-__version__ = "0.3.18.2"
+__version__ = "0.3.18.3"
 
 
 ### Temporary report for tree_interpretation_pipe
