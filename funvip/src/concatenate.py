e3ed223ad7b69d81fcfae66264b2aa141c991c13
Changwan Seo <wan101010@snu.ac.kr>
2024-07-25 16:48:14 +0900

    0.3.23.5.1.1
    - Just save for speed test with large dataset in the server

--- funvip/src/concatenate.py
+++ funvip/src/concatenate.py
@@ -156,9 +156,8 @@ def concatenate_df(V, path, opt):
         if isinstance(V.dict_gene_SR[gene], pd.DataFrame):
             gene_list.append(gene)
 
-            df = deepcopy(V.dict_gene_SR[gene].set_index(["qseqid", "sseqid"]))
+            # df = deepcopy(V.dict_gene_SR[gene].set_index(["qseqid", "sseqid"]))
 
-            """
             df = deepcopy(
                 V.dict_gene_SR[gene]
                 .set_index(["qseqid", "sseqid"])
@@ -173,12 +172,10 @@ def concatenate_df(V, path, opt):
                         "sstart",
                         "send",
                         "evalue",
-                        "bitscore",
-                        "subject_group",
                     ]
                 )
             )
-            """
+
             df_list.append(df)
 
     if len(df_list) <= 0:
@@ -202,7 +199,15 @@ def concatenate_df(V, path, opt):
         df_multigene_regression_ori = pd.concat(df_list, axis=1)
 
         # Drop unnecessary columns for processing
+        df_multigene_regression_ori.drop(
+            columns=[
+                "bitscore",
+                "subject_group",
+            ],
+            inplace=True,
+        )
 
+        """
         df_multigene_regression_ori.drop(
             columns=[
                 "pident",
@@ -219,6 +224,7 @@ def concatenate_df(V, path, opt):
             ],
             inplace=True,
         )
+        """
 
         # Column name managing on subject_group
         def same_merge(x, list_col):
